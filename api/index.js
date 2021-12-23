69630e1faff512e1700a1080371553d49720b0cb
Islahuddin41420110058 <islahuddindidin@gmail.com>
2021-12-23 11:51:22 +0700

    TERBARU

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cls_model = require('./sdk/cls_model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '5091733513:AAHCEuG_3zHV53IbEK3CHrVTIDur4piANL8'
+const token = '5091398105:AAHnFFJkaim8hYlIF9S_FlG0OWfwA702A-0'
 const bot = new TelegramBot(token, {polling: true});
 
 state = 0;
@@ -22,11 +22,11 @@ bot.onText(/\/start/, (msg) => {
     state = 0;
 });
 
-//input N dan B
+//input S dan K
 bot.onText(/\/predict/, (msg) => { 
     bot.sendMessage(
         msg.chat.id,
-        `input nilai suhu|jarak contohnya 25|10`
+        `input nilai S|K contohnya 30|50`
     );   
     state = 1;
 });
@@ -39,21 +39,21 @@ bot.on('message', (msg) => {
                 parseFloat(s[0]), // string to float
                 parseFloat(s[1])
             ]
-        ).then((dres1)=>{
-            console.log(dres1);
+        ).then((jres1)=>{
+            console.log(jres1);
             
-            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(dres1[0]), parseFloat(dres1[1])]).then((dres2)=>{
+            cls_model.classify([parseFloat(s[0]), parseFloat(s[1]), parseFloat(jres1[0]), parseFloat(jres1[1])]).then((jres2)=>{
                 bot.sendMessage(
                         msg.chat.id,
-                        `nilai suhu yang diprediksi adalah ${dres1[0]}`
+                        `nilai pompa yang diprediksi adalah ${jres1[0]}`
                 );
                 bot.sendMessage(
                         msg.chat.id,
-                        `nilai keadaanair yang diprediksi adalah ${dres1[1]}`
+                        `nilai kipas yang diprediksi adalah ${jres1[1]}`
                 ); 
                 bot.sendMessage(
                         msg.chat.id,
-                        `Klasifikasi ${dres2}`
+                        `Klasifikasi ${jres2}`
                 );
                 state = 0;
             })
@@ -67,56 +67,55 @@ bot.on('message', (msg) => {
     }
 })
 // routers
-r.get('/predict/:N/:B', function(req, res, next) {    
+r.get('/predict/:S/:K', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.N), // string to float
-            parseFloat(req.params.B)
+            parseFloat(req.params.S), // string to float
+            parseFloat(req.params.K)
         ]
-    ).then((dres)=>{
-        res.json(dres);
+    ).then((jres)=>{
+        res.json(jres);
     })
 });
 
 //routers
-r.get('/classify/:N/:B', function(req, res, next) {    
+r.get('/classify/:S/:K', function(req, res, next) {    
     model.predict(
         [
-            parseFloat(req.params.N), // string to float
-            parseFloat(req.params.B)
+            parseFloat(req.params.S), // string to float
+            parseFloat(req.params.K)
         ]
-    ).then((dres)=>{
+    ).then((jres)=>{
         cls_model.classify(
             [
-                parseFloat(req.params.N), // string to float
-                parseFloat(req.params.B),
-                parseFloat(dres[0]),
-                parseFloat(dres[1])
+                parseFloat(req.params.S), // string to float
+                parseFloat(req.params.K),
+                parseFloat(jres[0]),
+                parseFloat(jres[1])
             ]
-        ).then((dres_)=>{
-            let status = "MIST MAKER OFF AIR SEDANG DIISI";
+        ).then((jres_)=>{
+            let status = "POMPA OFF KIPAS ON";
             
-            if(dres_ == "1|1"){
-                status = "MIST MAKER ON AIR SEDANG DIISI"
-            }if(dres_ == "0|0"){
-                status = "MIST MAKER OFF AIR OFF "
-            }if(dres_ == "1|0"){
-                status = "MIST MAKER ON AIR OFF"
-            
+            if(jres_ == "1|1"){
+                status = "POMPA ON KIPAS ON"
+            }if(jres_ == "1|0"){
+                status = "POMPA ON KIPAS OFF"
+            }if(jres_ == "0|0"){
+                status = "POMPA OFF KIPAS OFF"
             }
             
-//             dres_.split("|");
-            const mistmaker = parseFloat(req.params.N);
-            const pengisianair = parseFloat(req.params.B)
+//             jres_.split("|");
+            const suhu = parseFloat(req.params.S);
+            const kelembaban = parseFloat(req.params.K)
            
             bot.sendMessage(
                     2128268907, //msg.id
-                    `mistmaker:: ${mistmaker} pengisianair ${pengisianair} KONDISI:: ${status}`
+                    `SUHU:: ${suhu} KELEMBABAN ${kelembaban} KONDISI:: ${status}`
                      
                      
             ); // to telegram
             
-            res.json({dres, dres_})
+            res.json({jres, jres_})
         })
     })
 });
--- api/sdk/model.js
+++ api/sdk/model.js
@@ -1,15 +1,15 @@
 const tf = require('@tensorflow/tfjs-node');
 
 function normalized(data){ // suhu dan kelembaban
-    N = (data[0] - 29.5) / 4.617796207  //29.5= avg    4.611213458 = stdev
-    B = (data[1] - 9.5) / 5.197158162
-    return [N, B]
+    S = (data[0] - 29.5) / 4.611213458  //29.5= avg    4.611213458 = stdev
+    K = (data[1] - 50.5) / 28.87509493 
+    return [S, K]
 }
 
 function denormalized(data){
-    M = (data[0] * 0.5625) + 0.496941867336809 
-    A = (data[1] * 0.722222222222222) + 0.448682848877996
-    return [M, A]
+    O = (data[0] * 0.4) + 0.490051113 // 0.497649258 = stdev  0.45 = avg
+    L = (data[1] * 0.496233468) + 0.5625
+    return [O, L]
 }
 
 
@@ -23,7 +23,7 @@ async function predict(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/PENGUKURANAIR/main/public/ex_model/model.json';
+        const path = 'https://raw.githubusercontent.com/Islahuddin41420110058/SKRIPSI/main/public/ex_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
